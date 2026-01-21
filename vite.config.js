1193e1d1219ec5f16e2f9789feff0f4193461583
akif-dev <akiffareed15@gmail.com>
2026-01-21 18:51:47 +0500

    Set base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Awami-Saholat-Website--local-Service-/', // <-- add this
 })
